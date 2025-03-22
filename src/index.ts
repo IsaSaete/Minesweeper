81b648075a1061dfb440c54b10b8966279a5b258
Isabel Sáenz <isasaete@gmail.comn>
2025-03-22 17:59:30 +0100

    Add pseudo-class :hover to the cell class selector

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { createBoard } from "./gameBoard/board/factory/createBoard.js";
 import { getRandomPositionMines } from "./gameBoard/mines/getPositionMines.js";
 import { renderBoard } from "./gameBoard/board/ui/renderBoard.js";
 
-const gameBoard = createBoard(6);
+const gameBoard = createBoard(8);
 const radonmMinesBoard = getRandomPositionMines(gameBoard);
 
 renderBoard(radonmMinesBoard);
